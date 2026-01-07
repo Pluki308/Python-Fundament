a2f610a77bed97feed263311c52b3a77d0d54e93
Pluki308 <pluk.vddonker@gmail.com>
2026-01-07 23:29:35 +0100

    half af

--- Opdrachten 8/5.py
+++ Opdrachten 8/5.py
@@ -1,15 +1,22 @@
 from appJar import gui
-import random
 
-def buttonClick():
-    chars=int(app.getSpinBox('nummers'))
-    klein=app.getCheckBox('Kleine letters')
-    groot=app.getCheckBox('Hoofdletters')
-    cijfer=app.getCheckBox('Cijfers')
-    teken=app.getCheckBox('Tekens')
-    app.setEntry('wachtwoord',f'{generate(chars,klein,groot,cijfer,teken)}')
+
+def buttonClick(naam):
+    global selected
+    if not app.getButton(naam):
+        if selected=='X':
+            app.setButton(naam,'X')
+            selected='O'
+        elif selected=='O':
+            app.setButton(naam,'O')
+            selected='X'
+    else:
+        app.setLabel('intro','Al ingevoerd.')
+        app.after(1000,lambda: app.setLabel('intro','TikTacToe'))
 
 def reset():
+    global selected
+    selected='X'
     app.setButton('box1','')
     app.setButton('box2','')
     app.setButton('box3','')
@@ -20,8 +27,16 @@ def reset():
     app.setButton('box8','')
     app.setButton('box9','')
 
+def selectX():
+    global selected
+    selected='X'
+def selectO():
+    global selected
+    selected='O'
+
 
 app = gui("Tafels", "300x150")
+
 
 
 app.setBg("#c7c7c7")
@@ -37,7 +52,10 @@ app.addButton('box7',buttonClick,1,2)
 app.addButton('box8',buttonClick,2,2)
 app.addButton('box9',buttonClick,3,2)
 
+reset()
 
+app.addButton("reset", reset,4,1)
+app.addButton("selectX", selectX,4,0)
+app.addButton("selectO", selectO,4,2)
 
-app.addButton("reset", reset,4,0,3)
 app.go()
